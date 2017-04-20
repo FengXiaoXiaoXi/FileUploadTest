6687378205273b2e129863de43f66d042ef25653
FengXiaoXiaoXi <1126070636@qq.com>
2017-04-20 22:40:51 +0800

    Update GitHubTest.java
    修改了文件

--- src/com/github/test/GitHubTest.java
+++ src/com/github/test/GitHubTest.java
@@ -9,6 +9,7 @@ public class GitHubTest {
 		System.out.println("Hello World"); 
 		System.out.println("Hello World"); 
 		System.out.println("Hello World"); 
+		System.out.println("我的文件已经更新了  看看你的本地仓库发生变化没有");
 
 	}
 
